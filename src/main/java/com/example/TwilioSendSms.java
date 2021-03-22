c1fb60de645c720a74b7a98b0b165d199a7107a5
Matthew Gilliard <matthew@gilliard.lol>
2021-03-22 12:06:06 +0000

    adds the code to send the SMS

--- src/main/java/com/example/TwilioSendSms.java
+++ src/main/java/com/example/TwilioSendSms.java
@@ -1,8 +1,21 @@
 package com.example;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 public class TwilioSendSms {
 
     public static void main(String[] args) {
-        // code will go in here
+
+        Twilio.init(
+            System.getenv("TWILIO_ACCOUNT_SID"),
+            System.getenv("TWILIO_AUTH_TOKEN"));
+
+        Message.creator(
+            new PhoneNumber("<TO number - ie your cellphone>"),
+            new PhoneNumber("<FROM number - ie your Twilio number"),
+            "Hello from Twilio 📞")
+            .create();
     }
 }
